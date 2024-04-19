1f0a5535a7951c51b9a88a6d560c142036e66821
k-a-webdev <kuzko.andrii.webdev@gmail.com>
2024-04-19 23:24:25 +0300

    build: code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import NotFound from "./pages/NotFound";
 import MainLayout from "./layouts/MainLayout";
-import FullPizza from "./pages/FullPizza";
 
 import "./scss/app.scss";
+
+const Cart = lazy(() => import(/* webpackChunkName: "Cart" */ "./pages/Cart"));
+const NotFound = lazy(
+  () => import(/* webpackChunkName: "NotFound" */ "./pages/NotFound")
+);
+const FullPizza = lazy(
+  () => import(/* webpackChunkName: "FullPizza" */ "./pages/FullPizza")
+);
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="/cart"
+          element={
+            <Suspense fallback="Loading....">
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/pizza/:id"
+          element={
+            <Suspense fallback="Loading....">
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback="Loading....">
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
@@ -25,3 +52,4 @@ export default function App() {
 // - change language to English
 // - change fonts to em/rem
 // - add more responsive styles
+// - add loading page
