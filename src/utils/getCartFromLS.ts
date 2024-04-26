7b2dd635d76af4e229c11f7570bc58b27e61e852
k-a-webdev <kuzko.andrii.webdev@gmail.com>
2024-04-26 14:41:09 +0300

    fix: getting incorrect data from the localstorage

--- src/utils/getCartFromLS.ts
+++ src/utils/getCartFromLS.ts
@@ -8,7 +8,7 @@ export const getCartFromLS = () => {
     (sum, el) => (sum += el.price * el.count),
     0
   );
-  const totalCount = products.reduce((sum, el) => (sum += 1), 0);
+  const totalCount = products.reduce((sum, el) => (sum += el.count), 0);
 
   return { products, totalPrice, totalCount };
 };
