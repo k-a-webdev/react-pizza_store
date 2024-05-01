cedbe7f655ee6de1dae8c845e11ffd54e2841cc1
k-a-webdev <kuzko.andrii.webdev@gmail.com>
2024-05-01 04:26:10 +0300

    Updates

---
+++ static/js/NotFound.c2dcf8b2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizza_store=self.webpackChunkpizza_store||[]).push([[187],{4949:(s,e,a)=>{a.r(e),a.d(e,{default:()=>p});var r=a(9747),t=a(184);const p=()=>(0,t.jsx)(r.vr,{})}}]);
+//# sourceMappingURL=NotFound.c2dcf8b2.chunk.js.map
--- static/js/NotFound.e5b7dde7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpizza_store=self.webpackChunkpizza_store||[]).push([[187],{4949:(s,e,a)=>{a.r(e),a.d(e,{default:()=>p});var r=a(1553),t=a(184);const p=()=>(0,t.jsx)(r.vr,{})}}]);
-//# sourceMappingURL=NotFound.e5b7dde7.chunk.js.map
